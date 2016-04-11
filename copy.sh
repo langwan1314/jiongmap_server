7ed437f32598c4f3f71979137de64d1764a07487
leaves <85121475@qq.com>
2016-04-11 07:51:55 -0700

    init

---
+++ copy.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+#start or stop the im-server
+cp login_server/login_server ../run/login_server/
+cp route_server/route_server ../run/route_server/
+cp msg_server/msg_server ../run/msg_server/
+cp http_msg_server/http_msg_server ../run/http_msg_server/
+cp file_server/file_server ../run/file_server/
+cp push_server/push_server ../run/push_server/
+cp db_proxy_server/db_proxy_server ../run/db_proxy_server/
+cp msfs/msfs ../run/msfs/
+cp tools/daeml ../run/
